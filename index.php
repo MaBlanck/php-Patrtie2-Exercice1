016de3011dea43fa0538d7c56f399f5e866752cf
Max <maxblanck@outlook.fr>
2020-12-08 11:34:47 +0100

    php Partie 2  Exercice 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 1 Partie 2</title>
+</head>
+<body>
+    
+    <?php
+    $age = 10;
+    if($age >= 18) 
+    {
+        echo "Vous êtes majeur";
+    } 
+    else 
+    {
+        echo "Vous êtes mineur";
+    };
+    ?>
+
+</body>
+</html>
