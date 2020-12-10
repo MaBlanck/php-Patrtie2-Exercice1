ee2e30777242a3832a9879279d8c6d69ace7f8bb
Max <maxblanck@outlook.fr>
2020-12-10 09:32:38 +0100

    php Partie 2  Exercice 1

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    $age = 18;
+?>
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
@@ -6,18 +10,11 @@
     <title>Exercice 1 Partie 2</title>
 </head>
 <body>
-    
-    <?php
-    $age = 10;
-    if($age >= 18) 
-    {
-        echo "Vous êtes majeur";
-    } 
-    else 
-    {
-        echo "Vous êtes mineur";
-    };
-    ?>
-
+    <h1>Exercice 1 Partie 2</h1> 
+    <?php  if ($age >= 18): ?>
+    <p><?= 'Vous êtes majeur'; ?></p>
+    <?php else :?>
+    <p><?= 'Vous êtes mineur'; ?></p>
+    <?php endif ;?>
 </body>
 </html>
